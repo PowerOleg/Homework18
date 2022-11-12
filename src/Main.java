7a7570100862612850a79010232fa311f2c02055
PowerOleg <geraskin.oleg@mail.ru>
2022-11-12 11:52:59 +0500

    Loop is done but doesn't work

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static List<Person> generateClients() {
         List<Person> queue = new LinkedList<>();
+        queue.add(new Person("Vladimir", "Popkov", 4));
+        queue.add(new Person("Petya", "Nikitin", 1));
+        queue.add(new Person("Nastya", "Shushukova", 2));
+        queue.add(new Person("Alena", "Vnutskih", 3));
+        queue.add(new Person("Alex", "Bui", 10));
         return queue;
     }
 
     public static void main(String[] args) {
-        List<Person> queue1 = new LinkedList<>();
+        Queue<Person> queue1 = new ArrayDeque<>(generateClients());
 
-        while (true) {
-            
+        boolean exit = false;
+        while (exit) {
+            int count = 0;
+            for (Iterator<Person> iterator = queue1.iterator(); iterator.hasNext(); ) {
+                Person p = iterator.next();
+                if (p.getTicketsNumber() > 0) {
+                    p.setTicketsNumber(p.getTicketsNumber() - 1);
+                    Person client = queue1.poll();
+                    System.out.printf("%s took a ride at the roller coaster\n", client);
+                    queue1.offer(client);
+                } else count++;
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+            if (queue1.size() == count) {
+                exit = true;
+            }
         }
 
     }
--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,26 @@ public class Person {
     private String name;
     private String surname;
     private int ticketsNumber;
+
+    public Person(String name, String surname, int ticketsNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.ticketsNumber = ticketsNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    public int getTicketsNumber() {
+        return ticketsNumber;
+    }
+
+    public void setTicketsNumber(int ticketsNumber) {
+        this.ticketsNumber = ticketsNumber;
+    }
 }
