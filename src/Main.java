a00f01c675373e76419006436fed864e9b425223
PowerOleg <geraskin.oleg@mail.ru>
2022-11-13 12:30:05 +0500

    ProgramLogic is done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+//        подумай может гдето запихать дженерики
+//        посмотри как у ков
+//из метода вынести создания объектов new
 import java.util.*;
 
 public class Main {
 
     public static List<Person> generateClients() {
         List<Person> queue = new LinkedList<>();
+        queue.add(new Person("Rabbit", "Hare", 0));
         queue.add(new Person("Vladimir", "Popkov", 4));
         queue.add(new Person("Petya", "Nikitin", 1));
         queue.add(new Person("Nastya", "Shushukova", 2));
@@ -11,31 +15,24 @@ public class Main {
         queue.add(new Person("Alex", "Bui", 10));
         return queue;
     }
-
+    public static void faceControl(Queue<Person> queue) {
+        for (Iterator<Person> iterator = queue.iterator(); iterator.hasNext(); ) {
+            Person p = iterator.next();
+            if (p.getTicketsNumber() <= 0) {
+                queue.poll();
+            } else {
+                p.setTicketsNumber(p.getTicketsNumber() - 1);
+                Person client = queue.poll();
+                System.out.printf("%s took a ride at the roller coaster (%d tickets left)\n",
+                        client, client.getTicketsNumber());
+                queue.offer(client);
+            }
+        }
+    }
     public static void main(String[] args) {
         Queue<Person> queue1 = new ArrayDeque<>(generateClients());
-
-        boolean exit = false;
-        while (exit) {
-            int count = 0;
-            for (Iterator<Person> iterator = queue1.iterator(); iterator.hasNext(); ) {
-                Person p = iterator.next();
-                if (p.getTicketsNumber() > 0) {
-                    p.setTicketsNumber(p.getTicketsNumber() - 1);
-                    Person client = queue1.poll();
-                    System.out.printf("%s took a ride at the roller coaster\n", client);
-                    queue1.offer(client);
-                } else count++;
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while (!queue1.isEmpty()) {
+            faceControl(queue1);
         }
-            if (queue1.size() == count) {
-                exit = true;
-            }
-        }
-
     }
 }
